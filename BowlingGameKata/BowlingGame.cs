57eb9ceda46f939cd63add165d3fb0b503988218
anaghakarurkar <anaghakarurkar@gmail.com>
2022-09-14 21:41:48 +0100

    Added Smallest part of the App - Bowling Turn class

---
+++ BowlingGameKata/BowlingGame.cs
@@ -0,0 +1,25 @@
+﻿namespace BowlingGameKata;
+public class BowlingGame
+{
+    private Frame[] _frames; 
+    public bool HasSteppedOnBlackLine;
+    private int _currentFrameID;
+    public BowlingGame()
+    {
+        _frames = new Frame[10];
+        HasSteppedOnBlackLine = false;
+        _currentFrameID = 0;
+    }
+
+    public void NextFrame()
+    {
+        _frames[_currentFrameID + 1] = new Frame();
+        _currentFrameID++;
+    }
+
+   
+
+
+
+
+}
---
+++ BowlingGameKata/BowlingTurn.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+
+namespace BowlingGameKata
+{
+    public class BowlingTurn
+    {
+       public int Score { get;   set; }
+    
+    }
+
+}
+
+
+
